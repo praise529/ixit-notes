3aa612cf8c243479789ffe34ecd57ae3ea5d4fff
Praise Olatope <aanuolatope@gmail.com>
2026-09-12 16:06:42 -0600

    Fix import path for ToolBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import DrawingCanvas from "./components/drawingcanvas";
-import { ToolBar } from "./components/toolbar";
+import { ToolBar } from "./components/ToolBar";
 import Stickie from "./components/stickie";
 
 export type tooltypes =
